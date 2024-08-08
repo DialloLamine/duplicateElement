44b138d5bffb1c13d1e11228c5b2d8e84cc1e911
Diallo <diallo.lamine11958@gmail.com>
2024-08-08 22:50:38 +0200

    find duplicate element from array

---
+++ duplicateNumber/src/duplicateNumber/findIt/FindDuplicateElementFromArray.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package duplicateNumber.findIt;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Diallo
+ * 
+ */
+public class FindDuplicateElementFromArray {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		int array[] = {1, 2, 3, 4, 2, 3, 7, 1};
+		
+		//int array[] = {1, 2, 3, 4, 2, 3, 7, 1, 2, 2};
+		
+		int count = 0;
+		
+		Set<Integer> duplicateElement = new HashSet<Integer>();
+		
+		for(int i = 0; i <array.length; i++) {
+			int elementThisPosition = array[i];
+			
+			count = 0;
+			for(int j = 0; j <array.length; j++) {
+				if(elementThisPosition == array[j]) {
+					count++;
+				}	
+			}
+			if(count > 1) {
+				duplicateElement.add(elementThisPosition);
+			}
+		}
+		System.out.println("Voici  les element qui ont des doublons. " + duplicateElement.toString());
+	}
+
+}
